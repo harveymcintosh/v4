fd26b93c0c1d11035097fdd6bf46d725583de564
HARVEY MCINTOSH <harveywmcintosh@outlook.com>
2022-02-03 13:18:40 +1000

    Test edit of hero section

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,12 +59,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Harvey McIntosh.</h2>;
+  const three = <h3 className="big-heading">I .</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
+        I’m a law studnet at the Queensland University of Technology. specializing in building (and occasionally designing) exceptional
         digital experiences. Currently, I’m focused on building accessible, human-centered products
         at{' '}
         <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
