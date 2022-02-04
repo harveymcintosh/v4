d8652d04b001a286f5ca6a585b38fea27bd5ebe1
HARVEY MCINTOSH <harveywmcintosh@outlook.com>
2022-02-04 19:18:31 +1000

    stuff

--- src/components/nav.js
+++ src/components/nav.js
@@ -172,11 +172,11 @@ const Nav = ({ isHome }) => {
     </div>
   );
 
-  const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
-      Resume
-    </a>
-  );
+  //const ResumeLink = (
+    //<a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+      //Resume
+    //</a>
+  //);
 
   return (
     <StyledHeader scrollDirection={scrollDirection} scrolledToTop={scrolledToTop}>
--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -136,34 +136,19 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Harvey
-              // and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Harvey and...
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              test {' '}
+              <a href="https://google.com/">url</a>
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              .
             </p>
 
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>test list:</p>
           </div>
 
           <ul className="skills-list">
