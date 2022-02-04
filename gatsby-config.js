8fd5204401ef752f68f4034378e0789afd93ead5
HARVEY MCINTOSH <harveywmcintosh@outlook.com>
2022-02-04 19:39:58 +1000

    uhh

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Harvey McIntosh',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Harvey McIntosh is a law student at the Queensland University of Technology.',
+    siteUrl: 'https://harveymcintosh.com.au', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@harveywmcintosh',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'HarveyMcIntosh',
+        short_name: 'HarveyMcIntosh',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
