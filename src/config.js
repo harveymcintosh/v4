44f6100ec1e02897a3f038aaa888ca7c5e4122f0
HARVEY MCINTOSH <harveywmcintosh@outlook.com>
2022-02-04 15:41:36 +1000

    general

--- src/config.js
+++ src/config.js
@@ -1,26 +1,10 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'harveywmcintosh@outlook.com',
 
   socialMedia: [
     {
-      name: 'GitHub',
-      url: 'https://github.com/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
-    },
-    {
       name: 'Linkedin',
       url: 'https://www.linkedin.com/in/bchiang7',
-    },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
     },
   ],
 
@@ -34,7 +18,7 @@ module.exports = {
       url: '/#jobs',
     },
     {
-      name: 'Work',
+      name: 'Skills',
       url: '/#projects',
     },
     {
