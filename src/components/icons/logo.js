0b84ea9621d1d37ffb968f93736cc5feadcbd31f
HARVEY MCINTOSH <harveywmcintosh@outlook.com>
2022-02-06 18:52:19 +1000

    commenting out translation and making bigger

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -3,15 +3,15 @@ import React from 'react';
 const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
     <title>Logo</title>
-    <g transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(11.000000, 5.000000)">
+    {/* <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)"> */}
       
     <defs>
         <style>@import url("https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i"); </style>
     </defs>
     <text fill="currentColor">
         {/* <tspan x="15" y="13.25">H</tspan> */}
-        <tspan x="39" y="45" font-size="x-large">H</tspan>
+        <tspan x="39" y="45" font-size="xxx-large">H</tspan>
     </text>
 
         {/* <path
